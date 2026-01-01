87817e5ce9fc98cb43c4c02f0c59fd3bffd3f439
kiganoakuma <daltonhberg@icloud.com>
2025-12-31 20:31:57 -0700

    day5 part2 completed

--- zig/solutions/2020/day5/src/main.zig
+++ zig/solutions/2020/day5/src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const print = std.debug.print;
+const expectEq = std.testing.expectEqual;
+const expect = std.testing.expect;
 
 const INPUT_FILE = @embedFile("input.txt");
 
@@ -47,7 +49,6 @@ fn decodeSeat(code: []const u8) usize {
     var low: usize = 0;
     var high: usize = 7;
     for (code) |c| {
-        print("c: {c}\n", .{c});
         const mid = (low + high) / 2;
         if (c == 'L') {
             high = mid;
@@ -60,7 +61,9 @@ fn decodeSeat(code: []const u8) usize {
     return low;
 }
 
-fn seatId(row_data: []const u8, seat_data: []const u8) usize {
+fn seatId(input: Input) usize {
+    const row_data = input.row_data;
+    const seat_data = input.seat_data;
     const row = decodeRow(row_data);
     const seat = decodeSeat(seat_data);
     return (row * 8) + seat;
@@ -72,22 +75,64 @@ pub fn main() !void {
     const data_sets = try parseInput(alloc.allocator(), INPUT_FILE);
     defer alloc.allocator().free(data_sets);
 
-    const highest_seat_id = try part1(data_sets);
-    print("highest seat id: {}\n", .{highest_seat_id});
+    // const highest_seat_id = try part1(data_sets);
+    // print("highest seat id: {}\n", .{highest_seat_id});
+    const my_set = try part2(data_sets);
+    print("my seat: {}\n", .{my_set});
 }
 
 fn part1(data_sets: []Input) !usize {
     var highest_seat_id: usize = 0;
     for (data_sets) |data_set| {
-        var row_data = data_set.row_data;
-        row_data = std.mem.trim(u8, row_data, " ");
-        var seat_data = data_set.seat_data;
-        seat_data = std.mem.trim(u8, seat_data, " ");
-
-        const seat_id = seatId(row_data, seat_data);
+        const seat_id = seatId(data_set);
         if (seat_id > highest_seat_id) {
             highest_seat_id = seat_id;
         }
     }
     return highest_seat_id;
+}
+
+fn part2(data_sets: []Input) !usize {
+    var taken = [_]bool{false} ** (128 * 8);
+
+    for (data_sets) |data_set| {
+        const seat_id = seatId(data_set);
+        taken[seat_id] = true;
+    }
+
+    var idx: usize = 1;
+    while (idx < taken.len - 1) : (idx += 1) {
+        const right = taken[idx - 1];
+        const possible = taken[idx];
+        const left = taken[idx + 1];
+
+        if (right and !possible and left) {
+            return idx;
+        }
+    }
+    return error.NoSeatFound;
+}
+
+test "part1" {
+    const test_alloc = std.testing.allocator;
+
+    const input = @embedFile("input.txt");
+    const input_data_sets = try parseInput(test_alloc, input);
+    defer test_alloc.free(input_data_sets);
+
+    const high_seat_id = try part1(input_data_sets);
+
+    try expectEq(842, high_seat_id);
+}
+
+test "part2" {
+    const test_alloc = std.testing.allocator;
+
+    const input = @embedFile("input.txt");
+    const input_data_sets = try parseInput(test_alloc, input);
+    defer test_alloc.free(input_data_sets);
+
+    const my_seat = try part2(input_data_sets);
+
+    try expectEq(617, my_seat);
 }
