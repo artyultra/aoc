3f21176e672fad321b92aea3f2c16c41a477c0ac
kiganoakuma <daltonhberg@icloud.com>
2026-03-05 19:12:10 -0700

    rust 2017 day11 completed

--- rust/solutions/2017/day11/src/main.rs
+++ rust/solutions/2017/day11/src/main.rs
@@ -1,6 +1,51 @@
 use std::fs;
 
+fn solve(path: Vec<&str>) -> (i32, i32) {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut z: i32 = 0;
+    let mut max_dist = 0;
+    for dir in path {
+        match dir {
+            "n" => {
+                y += 1;
+                z -= 1;
+            }
+            "ne" => {
+                x += 1;
+                z -= 1;
+            }
+            "se" => {
+                x += 1;
+                y -= 1;
+            }
+            "s" => {
+                y -= 1;
+                z += 1;
+            }
+            "sw" => {
+                x -= 1;
+                z += 1;
+            }
+            "nw" => {
+                x -= 1;
+                y += 1;
+            }
+            _ => panic!("Invalid direction {dir}"),
+        }
+        let dist = x.abs().max(y.abs()).max(z.abs());
+        if dist > max_dist {
+            max_dist = dist;
+        }
+    }
+    let final_dist = x.abs().max(y.abs()).max(z.abs());
+    (final_dist, max_dist)
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap().trim().to_string();
-    println!("{}", input);
+    let path = input.split(",").collect::<Vec<&str>>();
+    let (p1, p2) = solve(path);
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 }
